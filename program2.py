a5d8cca30a9751a1e14d090fa7ea88cfbe2fbe85
muskandevnani <muskandevnani028@gmail.com>
2024-09-27 14:33:35 +0530

    program2.py

--- program2.py
+++ program2.py
@@ -1,10 +1,35 @@
-class Solution(object):
-    def romanToInt(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        pass
+def romanToInt(s: str) -> int:
+    # Mapping of Roman numerals to integers
+    roman_to_int = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+        'M': 1000
+    }
+    
+    total = 0
+    prev_value = 0
+    
+    # Iterate over each character in the string from right to left
+    for char in reversed(s):
+        current_value = roman_to_int[char]
+        
+        # If the current value is less than the previous value, subtract it
+        if current_value < prev_value:
+            total -= current_value
+        else:
+            # Otherwise, add it to the total
+            total += current_value
+        
+        # Update the previous value to the current one
+        prev_value = current_value
+        
+    return total
 
-
-
+# Test cases
+print(romanToInt("III"))      # Output: 3
+print(romanToInt("LVIII"))    # Output: 58
+print(romanToInt("MCMXCIV"))  # Output: 1994
